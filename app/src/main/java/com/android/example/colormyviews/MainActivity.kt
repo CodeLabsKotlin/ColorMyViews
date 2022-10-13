4834202be3f5f842692034ed1f92a3afa43fa158
Nahima Ortega <nahimaortega@gmail.com>
2022-10-13 23:27:46 +0100

    added click handlers to buttons

--- app/src/main/java/com/android/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/android/example/colormyviews/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -14,12 +15,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeColored(view: View) {
+
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+
         when (view.id) {
             R.id.box_one_text -> view.setBackgroundColor(Color.CYAN)
             R.id.box_two_text -> view.setBackgroundColor(Color.GREEN)
             R.id.box_three_text -> view.setBackgroundColor(Color.YELLOW)
             R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_five_text -> view.setBackgroundColor(Color.RED)
+
+
+            R.id.red_button -> boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> boxFiveText.setBackgroundResource(R.color.my_green)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
@@ -34,9 +46,15 @@ class MainActivity : AppCompatActivity() {
 
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
+        val redButton = findViewById<Button>(R.id.red_button)
+        val greenButton = findViewById<Button>(R.id.green_button)
+        val yellowButton = findViewById<Button>(R.id.yellow_button)
+
         val clickableViews: List<View> =
             listOf(boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout)
+                boxFourText, boxFiveText, rootConstraintLayout,
+                redButton, greenButton, yellowButton
+            )
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
